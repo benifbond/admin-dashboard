2fbd94146c4865e0d85d46e007581151d8f989af
Ifeanyi Beniah Onyebueke <ifeanyionyebueke.ben@gmail.com>
2022-10-25 18:00:19 +0200

    incomplete theming

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ function App() {
             </div>
 
           <div>
+              <ThemeSettings />
             <Routes>
               {/* Dashboard */}
               <Route path='/' element={<Ecommerce />} />
